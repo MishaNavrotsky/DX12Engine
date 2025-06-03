39e7ac172617c8683a7098d3be69d21da892d212
Misha <mi.navrotskyy@gmail.com>
2025-06-03 19:28:57 +0300

    More use of std::optional

--- Engine/lib/scene/assets/mesh/AssetMesh.h
+++ Engine/lib/scene/assets/mesh/AssetMesh.h
@@ -9,11 +9,11 @@
 namespace Engine::Scene::Asset {
 	struct CPUAttributeData {
 		Render::VertexAttribute attribute;
-		std::vector<std::byte> data;
+		std::optional<std::vector<std::byte>> data;
 	};
 	struct CPUDataSubMesh {
 		std::vector<CPUAttributeData> attributes;
-		std::vector<std::byte> indicesData;
+		std::optional<std::vector<std::byte>> indicesData;
 		DXGI_FORMAT indicesFormat;
 		uint64_t indicesSizeInBytes;
 		uint64_t totalCPUSizeInBytes;
@@ -21,12 +21,12 @@ namespace Engine::Scene::Asset {
 
 	struct GPUAttributeData {
 		Render::VertexAttribute attribute;
-		Render::Memory::Resource::PackedHandle resourceId;
-		D3D12_GPU_VIRTUAL_ADDRESS gpuVirtualAddress;
+		std::optional<Render::Memory::Resource::PackedHandle> resourceId;
+		std::optional<D3D12_GPU_VIRTUAL_ADDRESS> gpuVirtualAddress;
 	};
 	struct GPUDataSubMesh {
-		Render::Memory::Resource::PackedHandle indexResourceId;
-		D3D12_GPU_VIRTUAL_ADDRESS indexGpuVirtualAddress;
+		std::optional<Render::Memory::Resource::PackedHandle> indexResourceId;
+		std::optional<D3D12_GPU_VIRTUAL_ADDRESS> indexGpuVirtualAddress;
 		DXGI_FORMAT indicesFormat;
 		std::vector<GPUAttributeData> attributes;
 		uint64_t indicesSizeInBytes;
